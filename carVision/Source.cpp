869696eeaa9cde660998fcc09a2573654d96f2d8
osipp <osipp@HP>
2017-03-02 15:46:39 +0700

    almost ready

--- carVision/Source.cpp
+++ carVision/Source.cpp
@@ -37,17 +37,18 @@ newvid:
 
 	Mat edge, gray2, img2, draw;
 	int x = 200;
-	int y = 426,ynew=0;
+	int y = 426, ynew = 0;
 	int defx = 200, defy = 426;
 	int width = 325;//225
 	int height = 54;
-	int block = 0, oldD1=0, oldD2=0, oldD3=0, oldD4=0;
+	int block = 0, oldD1 = 0, oldD2 = 0, oldD3 = 0, oldD4 = 0;
 	int xcheck, ycheck;
 	Mat orig, origDot;
-	int blDotX, blDotX2, blDotX3, blDotX4, countFrame = 0, start=0;
+	int blDotX, blDotX2, blDotX3, blDotX4, countFrame = 0, start = 0;
 	int Xco, Xco2;
 	uint8_t *row, *row2, *row3;
 	int frameCount = 0;
+	int widthlinetop=85, widthlinebot = 90;
 
 	while (1) {
 		if (x < 0 || x>640) x = defx;
@@ -61,8 +62,6 @@ newvid:
 			if (verb == 'r') goto newvid;
 		}
 		dst = cvCloneImage(frame);
-		//dst2 = cvCloneImage(frame);
-		//
 		float kernel[9];
 		kernel[0] = 0.1;
 		kernel[1] = 0.1;
@@ -78,13 +77,11 @@ newvid:
 
 		CvMat kernel_matrix = cvMat(3, 3, CV_32FC1, kernel);
 		cvFilter2D(dst, dst, &kernel_matrix, cvPoint(-1, -1));
-		//
 		cvSetImageROI(dst, cvRect(x, y, width, height));
 		cvSetImageROI(frame, cvRect(x, y, width, height));
-		cvAddS(frame, cvScalar(123,123,123), frame);
+		cvAddS(frame, cvScalar(123, 123, 123), frame);
 		cvResetImageROI(frame);
 		orig = cvarrToMat(dst);
-		//cvSetImageROI(dst2, cvRect(x, y, width, height));
 		orig = cvarrToMat(dst);
 		cont = cvCloneImage(&(IplImage)orig);
 		row = (uint8_t*)orig.ptr<uint8_t>(1);
@@ -107,49 +104,50 @@ newvid:
 		blDotX2 = width - 1;
 		blDotX3 = 0;
 		blDotX4 = width - 1;
-			while (!isBlack(blDotX, row))
-				blDotX++;
-			if (frameCount > 3) if (abs(blDotX - oldD1) > 70) blDotX = oldD1;
-			cvCircle(frame, cvPoint(x + blDotX, y + 1), 3, cvScalar(255, 0, 0));
-			while (status1 == 0)
-			{
-				if (blDotX2 <= 0) {
-					blDotX2 = blDotX; status1 = 1;
-				}
-				if (isBlack(blDotX2, row))
-				{
-					if (blDotX2 < 10) break;
-					if (blDotX2 - blDotX <= 50)
-						status1 = 1;
-				}
-				if (status1 == 0) blDotX2--;
+		while (!isBlack(blDotX, row))
+			blDotX++;
+		if (frameCount > 3) if (abs(blDotX - oldD1) > 70) blDotX = oldD1;
+		cvCircle(frame, cvPoint(x + blDotX, y + 1), 3, cvScalar(255, 0, 0));
+		while (status1 == 0)
+		{
+			if (blDotX2 <= 0) {
+				blDotX2 = blDotX; status1 = 1;
 			}
-			if (frameCount > 3)if (abs(blDotX2 - oldD2) > 70) blDotX2 = oldD2;
-			cvCircle(frame, cvPoint(x + blDotX2, y + 1), 3, cvScalar(155, 155, 155));
-			while (!isBlack(blDotX3, row2))
-				blDotX3++;
-			if (frameCount > 3)if (abs(blDotX3 - oldD3) > 70) blDotX3 = oldD3;
-			cvCircle(frame, cvPoint(x + blDotX3, y + 48), 3, cvScalar(0, 0, 255));
-			while (status2 == 0)
+			if (isBlack(blDotX2, row))
 			{
-				if (blDotX4 <= 0) {
-					blDotX4 = blDotX3; status2 = 1;
-				}
-				if (isBlack(blDotX4, row2))
-				{
-					if (blDotX4 < 10) break;
-					if (blDotX4 - blDotX3 <= 65)
-					{
-						status2 = 1;
-					}
-				}
-				if (status2 == 0) blDotX4--;
+				if (blDotX2 < 10) break;
+				if (blDotX2 - blDotX <= widthlinetop)
+					status1 = 1;
 			}
-			if (!isBlack(blDotX, row)) blDotX = 120;
-			if (!isBlack(blDotX2, row)) blDotX2 = 170;
-			if (!isBlack(blDotX3, row2)) blDotX3 = 115;
-			if (!isBlack(blDotX4, row2)) blDotX4 = 175;
-
+			if (status1 == 0) blDotX2--;
+		}
+		if (frameCount > 3)if (abs(blDotX2 - oldD2) > 70) blDotX2 = oldD2;
+		cvCircle(frame, cvPoint(x + blDotX2, y + 1), 3, cvScalar(155, 155, 155));
+		while (!isBlack(blDotX3, row2))
+			blDotX3++;
+		if (frameCount > 3)if (abs(blDotX3 - oldD3) > 70) blDotX3 = oldD3;
+		cvCircle(frame, cvPoint(x + blDotX3, y + 48), 3, cvScalar(0, 0, 255));
+		while (status2 == 0)
+		{
+			if (blDotX4 <= 0) {
+				blDotX4 = blDotX3; status2 = 1;
+			}
+			if (isBlack(blDotX4, row2))
+			{
+				if (blDotX4 < 10) break;
+				if (blDotX4 - blDotX3 <= widthlinebot)
+				{
+					status2 = 1;
+				}
+			}
+			if (status2 == 0) blDotX4--;
+		}
+		if ((abs(blDotX - blDotX3) > 30 && abs(blDotX - blDotX3) < 51) || (abs(blDotX4 - blDotX2) > 58 || abs(blDotX4 - blDotX2) < 80)) widthlinetop = 70;
+		if ((abs(blDotX - blDotX3) > 6 && abs(blDotX - blDotX3) < 31) || (abs(blDotX4 - blDotX2) > 48 || abs(blDotX4 - blDotX2) < 59)) widthlinetop = 58;
+		if (!isBlack(blDotX, row)) blDotX = 120;
+		if (!isBlack(blDotX2, row)) blDotX2 = 170;
+		if (!isBlack(blDotX3, row2)) blDotX3 = 115;
+		if (!isBlack(blDotX4, row2)) blDotX4 = 175;
 		if (frameCount>3)if (abs(blDotX4 - oldD4) > 70) blDotX4 = oldD4;
 		if (blDotX - blDotX3 > 110) start = 1;
 		cvCircle(frame, cvPoint(x + blDotX4, y + 48), 3, cvScalar(0, 255, 0));
@@ -171,34 +169,30 @@ newvid:
 		if (contours)
 		{
 			contours = cvApproxPoly(contours, sizeof(CvContour), storage, CV_POLY_APPROX_DP, 10, 1);
-
 			for (CvSeq* seq0 = contours; seq0 != 0; seq0 = seq0->h_next) {
 				CvPoint* coord = (CvPoint*)cvGetSeqElem(contours, 1);
-					double squ = cvContourArea(seq0);
-					cout << "squere=" << squ << endl;
-					double perim = cvContourPerimeter(seq0);
-					cout << "perim:" << perim << endl;
-
-
-					if (perim < 95 && perim>10) {
-						coord = (CvPoint*)cvGetSeqElem(contours, 1);
-						xcheck = coord[0].x;
-						row3 = (uint8_t*)orig.ptr<uint8_t>(coord[0].y);
-						if (frameCount > 20)
-						{
-								block = 1;
-							if (block == 1) {
-								cvDrawContours(cont, seq0, CV_RGB(255, 0, 0), CV_RGB(0, 0, 250), 1, 2, 8);
-							}
+				double squ = cvContourArea(seq0);
+				cout << "squere=" << squ << endl;
+				double perim = cvContourPerimeter(seq0);
+				cout << "perim:" << perim << endl;
+				if (perim < 80 && perim>10) {
+					coord = (CvPoint*)cvGetSeqElem(contours, 1);
+					xcheck = coord[0].x;
+					row3 = (uint8_t*)orig.ptr<uint8_t>(coord[0].y);
+					if (frameCount > 20)
+					{
+						block = 1;
+						if (block == 1) {
+							cvDrawContours(cont, seq0, CV_RGB(255, 0, 0), CV_RGB(0, 0, 250), 1, 2, 8);
+						}
 						cvSetImageROI(frame, cvRect(x, y, width, height));
 						cvAddS(cont, cvScalar(250), frame);
 						cvResetImageROI(frame);
 					}
 				}
-				cvWaitKey(10);
+				cvWaitKey(30);
 				cvShowImage(names[u], frame);
 			}
-		
 		}
 		else {
 			cvShowImage(names[u], frame);
